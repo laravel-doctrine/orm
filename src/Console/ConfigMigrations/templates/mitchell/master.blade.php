d3f1db6d8d2c2c868bbb6645a896ea951f29dcb6
FoxxMD <matt.duncan13@gmail.com>
2015-07-16 09:51:01 -0400

    Include config comments in generated configuration

--- src/Console/ConfigMigrations/templates/mitchell/master.blade.php
+++ src/Console/ConfigMigrations/templates/mitchell/master.blade.php
@@ -1,11 +1,33 @@
 return [
+        /*
+        |--------------------------------------------------------------------------
+        | Development state
+        |--------------------------------------------------------------------------
+        |
+        | If set to false, metadata caching will become active
+        |
+        */
         'dev'                       => config('app.debug'),
+        /*
+        |--------------------------------------------------------------------------
+        | Entity Mangers
+        |--------------------------------------------------------------------------
+        |
+        */
         'managers'                  => [
                 @foreach($managers as $key => $manager)
                     '{{$key}}' => {{$manager}},
                     @endforeach
 
         ],
+        /*
+        |--------------------------------------------------------------------------
+        | Doctrine Meta Data
+        |--------------------------------------------------------------------------
+        |
+        | Available: annotations|yaml|xml
+        |
+        */
         'meta'                      => [
                 'namespaces' => [
                         'App'
@@ -30,15 +52,44 @@ return [
                         ]
                 ]
         ],
+        /*
+        |--------------------------------------------------------------------------
+        | Doctrine Extensions
+        |--------------------------------------------------------------------------
+        |
+        | Enable/disable Doctrine Extensions by adding or removing them from the list
+        |
+        */
         'extensions'                => [
             //LaravelDoctrine\ORM\Extensions\TablePrefix\TablePrefixExtension::class,
         ],
+        /*
+        |--------------------------------------------------------------------------
+        | Doctrine custom types
+        |--------------------------------------------------------------------------
+        */
         'custom_types'              => [
                 'json' => LaravelDoctrine\ORM\Types\Json::class
         ],
         @if($dqls !== null)
             {{$dqls}}
             @endif
+        /*
+        |--------------------------------------------------------------------------
+        | Enable Debugbar Doctrine query collection
+        |--------------------------------------------------------------------------
+        */
         'debugbar'                  => env('DOCTRINE_DEBUGBAR', false),
+        /*
+        |--------------------------------------------------------------------------
+        | Cache
+        |--------------------------------------------------------------------------
+        |
+        | By default the Laravel cache setting is used,
+        | but it's possible to overrule here
+        |
+        | Available: acp|array|file|memcached|redis
+        |
+        */
         {{$cache}}
 ];
