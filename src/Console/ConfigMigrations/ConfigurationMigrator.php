3663abfbe482f571db09b22787f4f4c33305cfa6
FoxxMD <matt.duncan13@gmail.com>
2015-07-21 09:47:03 -0400

    Specify constructor parameters in ConfigurationMigration interface

--- src/Console/ConfigMigrations/ConfigurationMigrator.php
+++ src/Console/ConfigMigrations/ConfigurationMigrator.php
@@ -2,8 +2,15 @@
 
 namespace LaravelDoctrine\ORM\ConfigMigrations;
 
+use Illuminate\Contracts\View\Factory;
+
 interface ConfigurationMigrator
 {
+    /**
+     * @param Factory $viewFactory Laravel View Factory used to render blade templates
+     */
+    public function __construct(Factory $viewFactory);
+
     /**
      * Convert a configuration array from another laravel-doctrine project in to a string representation of a php array configuration for this project
      *
