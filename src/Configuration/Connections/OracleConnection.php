a67d5e7f6658d8319c85295a0d38c9d3c4fde896
gmsantosxl <gabriel.ms1@hotmail.com>
2015-07-30 16:43:55 -0300

    create oracle driver connection

---
+++ src/Configuration/Connections/OracleConnection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LaravelDoctrine\ORM\Configuration\Connections;
+
+class OracleConnection extends AbstractConnection
+{
+    /**
+     * @var string
+     */
+    protected $name = 'oracle';
+
+    /**
+     * @param array $config
+     *
+     * @return OracleConnection
+     */
+    public function configure($config = [])
+    {
+        return new static ([
+            'driver'   => 'oci8',
+            'host'     => array_get($config, 'host'),
+            'dbname'   => array_get($config, 'database'),
+            'user'     => array_get($config, 'username'),
+            'password' => array_get($config, 'password'),
+            'prefix'   => array_get($config, 'prefix'),
+            'port'     => array_get($config, 'port'),
+        ]);
+    }
+}
