102c3a320650ae7d16c9ab549fb62a1d8388dffe
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-02-12 19:03:52 +0100

    Add convert mapping command

--- src/DoctrineServiceProvider.php
+++ src/DoctrineServiceProvider.php
@@ -20,6 +20,7 @@ use LaravelDoctrine\ORM\Console\ClearMetadataCacheCommand;
 use LaravelDoctrine\ORM\Console\ClearQueryCacheCommand;
 use LaravelDoctrine\ORM\Console\ClearResultCacheCommand;
 use LaravelDoctrine\ORM\Console\ConvertConfigCommand;
+use LaravelDoctrine\ORM\Console\ConvertMappingCommand;
 use LaravelDoctrine\ORM\Console\EnsureProductionSettingsCommand;
 use LaravelDoctrine\ORM\Console\GenerateEntitiesCommand;
 use LaravelDoctrine\ORM\Console\GenerateProxiesCommand;
@@ -301,7 +302,8 @@ class DoctrineServiceProvider extends ServiceProvider
             GenerateProxiesCommand::class,
             ConvertConfigCommand::class,
             MappingImportCommand::class,
-            GenerateEntitiesCommand::class
+            GenerateEntitiesCommand::class,
+            ConvertMappingCommand::class
         ]);
     }
 
