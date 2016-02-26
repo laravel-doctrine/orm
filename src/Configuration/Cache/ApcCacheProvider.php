987aeacd8ba2d2bdcc84b36745e206c9474c281c
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-02-26 13:10:32 +0100

    Cs fixes

--- src/Configuration/Cache/ApcCacheProvider.php
+++ src/Configuration/Cache/ApcCacheProvider.php
@@ -2,33 +2,10 @@
 
 namespace LaravelDoctrine\ORM\Configuration\Cache;
 
-use Illuminate\Contracts\Cache\Factory;
-use LaravelDoctrine\ORM\Configuration\Driver;
-
-class ApcCacheProvider implements Driver
+class ApcCacheProvider extends IlluminateCacheProvider
 {
     /**
-     * @var Factory
+     * @var string
      */
-    protected $cache;
-
-    /**
-     * @param Factory $cache
-     */
-    public function __construct(Factory $cache)
-    {
-        $this->cache = $cache;
-    }
-
-    /**
-     * @param array $settings
-     *
-     * @return ApcCache
-     */
-    public function resolve(array $settings = [])
-    {
-        return new IlluminateCacheAdapter(
-            $this->cache->store('apc')
-        );
-    }
+    protected $store = 'apc';
 }
--- src/Configuration/Cache/MemcachedCacheProvider.php
+++ src/Configuration/Cache/MemcachedCacheProvider.php
@@ -2,33 +2,10 @@
 
 namespace LaravelDoctrine\ORM\Configuration\Cache;
 
-use Illuminate\Contracts\Cache\Factory;
-use LaravelDoctrine\ORM\Configuration\Driver;
-
-class MemcachedCacheProvider implements Driver
+class MemcachedCacheProvider extends IlluminateCacheProvider
 {
     /**
-     * @var Factory
+     * @var string
      */
-    protected $cache;
-
-    /**
-     * @param Factory $cache
-     */
-    public function __construct(Factory $cache)
-    {
-        $this->cache = $cache;
-    }
-
-    /**
-     * @param array $settings
-     *
-     * @return MemcachedCache
-     */
-    public function resolve(array $settings = [])
-    {
-        return new IlluminateCacheAdapter(
-            $this->cache->store('memcached')
-        );
-    }
+    protected $store = 'memcached';
 }
