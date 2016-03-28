359c0b6e65788c3ad9cbaa7842bd5a27c6354a20
Amitai Lanciano <tai@lanayo.com>
2016-03-28 16:38:14 -0700

    change default path to current working directory, and use argument path if provided

--- src/Console/GenerateEntitiesCommand.php
+++ src/Console/GenerateEntitiesCommand.php
@@ -52,7 +52,7 @@ class GenerateEntitiesCommand extends Command
             $metadatas = $cmf->getAllMetadata();
             $metadatas = MetadataFilter::filter($metadatas, $this->option('filter'));
 
-            $destPath = base_path('app/Entities');
+            $destPath = base_path($this->argument('dest-path') ? $this->argument('dest-path') : '.');
 
             if (!is_dir($destPath)) {
                 mkdir($destPath, 0777, true);
