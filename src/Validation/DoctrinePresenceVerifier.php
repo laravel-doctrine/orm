214f3f892058f1ca6a0303ca292da0f531065cdd
José Carlos <josecarlos@globtec.com.br>
2016-05-16 17:58:34 -0300

    Fix the bug "Invalid PathExpression" in validation service
    Showed when the validation service was used with Doctrine version 2.5.4.
    In the expression COUNT, was used the entity alias.

--- src/Validation/DoctrinePresenceVerifier.php
+++ src/Validation/DoctrinePresenceVerifier.php
@@ -92,7 +92,7 @@ class DoctrinePresenceVerifier implements PresenceVerifierInterface
         $em      = $this->getEntityManager($collection);
         $builder = $em->createQueryBuilder();
 
-        $builder->select('count(e)')->from($collection, 'e');
+        $builder->select('count(1)')->from($collection, 'e');
 
         return $builder;
     }
