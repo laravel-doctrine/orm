laravel-doctrine/orm

commit ef172475c419f35c2a98086de925b04f8f869b68

[FEATURE] Add support for multiple cache implementations, separating result, query and metadata caching. (#205)
* Implement multiple cache driver support in EntityManagerFactory

* Make all EntityManagerFactory tests pass (may not be correct yet)

* Refactor the EntityManagerFactory to properly make use of cache drivers and second level caching

* Fix unit tests for EntityManagerFactory to reflect new EMF code

* Fix the default cache implementation to ensure backwards compatibility

* Fix formatting for StyleCI build

* Add more formatting for StyleCI

* Add even more StyleCI fixes

* Change multicaching to use driver instead of type in the cache array

Also added some docblock comments

* StyleCI will be the death of me

src/EntityManagerFactory.php
@@ -58,6 +58,14 @@ class EntityManagerFactory
     private $resolver;
 
     /**
+     * @var array
+     */
+    private $defaultCache = [
+        'type'      => 'array',
+        'namespace' => null
+    ];
+
+    /**
      * @param Container              $container
      * @param Setup                  $setup
      * @param MetaDataManager        $meta
@@ -93,8 +101,7 @@ class EntityManagerFactory
     {
         $configuration = $this->setup->createConfiguration(
             array_get($settings, 'dev', false),
-            array_get($settings, 'proxies.path'),
-            $this->cache->driver()
+            array_get($settings, 'proxies.path')
         );
 
         $this->setMetadataDriver($settings, $configuration);
@@ -318,11 +325,29 @@ class EntityManagerFactory
      */
     protected function setCacheSettings(Configuration $configuration)
     {
-        if ($namespace = $this->config->get('doctrine.cache.namespace', null)) {
-            $this->cache->driver()->setNamespace($namespace);
-        }
+        $configuration->setQueryCacheImpl($this->applyNamedCacheConfiguration('query'));
+        $configuration->setResultCacheImpl($this->applyNamedCacheConfiguration('result'));
+        $configuration->setMetadataCacheImpl($this->applyNamedCacheConfiguration('metadata'));
 
         $this->setSecondLevelCaching($configuration);
+    }
+
+    /**
+     * @param  string $cacheName
+     * @return mixed
+     */
+    private function applyNamedCacheConfiguration($cacheName)
+    {
+        $defaultDriver    = $this->config->get('doctrine.cache.default', $this->defaultCache['type']);
+        $defaultNamespace = $this->config->get('doctrine.cache.namespace', $this->defaultCache['namespace']);
+
+        $driver = $this->config->get('doctrine.cache.' . $cacheName . '.driver', $defaultDriver);
+
+        if ($namespace = $this->config->get('doctrine.cache.' . $cacheName . '.namespace', $defaultNamespace)) {
+            $this->cache->driver($driver)->setNamespace($namespace);
+        }
+
+        return $this->cache->driver($driver);
     }
 
     /**

config/doctrine.php
@@ -154,10 +154,22 @@ return [
     | Available: apc|array|file|memcached|redis|void
     |
     */
-    'cache'                      => [
-        'default'      => env('DOCTRINE_CACHE', 'array'),
-        'namespace'    => null,
-        'second_level' => false,
+    'cache' => [
+        'second_level'     => false,
+        'default'          => 'array',
+        'namespace'        => null,
+        'metadata'         => [
+            'driver'       => env('DOCTRINE_METADATA_CACHE', 'array'),
+            'namespace'    => null,
+        ],
+        'query'            => [
+            'driver'       => env('DOCTRINE_QUERY_CACHE', 'array'),
+            'namespace'    => null,
+        ],
+        'result'           => [
+            'driver'       => env('DOCTRINE_RESULT_CACHE', 'array'),
+            'namespace'    => null,
+        ],
     ],
     /*
     |--------------------------------------------------------------------------

tests/EntityManagerFactoryTest.php
@@ -77,6 +77,11 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
     /**
      * @var array
      */
+    protected $caches = [ 'query', 'result', 'metadata' ];
+
+    /**
+     * @var array
+     */
     protected $settings = [
         'meta'       => 'annotations',
         'connection' => 'mysql',
@@ -91,8 +96,19 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
 
     protected $cacheImpl;
 
+    /**
+     * @Notes This just holds the length of $caches
+     *        It's just here so we don't have to call count($caches) over and over.
+     * @var int
+     */
+    private $cachesCount;
+
     protected function setUp()
     {
+
+        //Just store the count of caches
+        $this->cachesCount = count($this->caches);
+
         $this->mockApp();
         $this->mockMeta();
         $this->mockConnection();
@@ -207,7 +223,8 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
                             ->atLeast()->once()->andReturn($cacheConfig);
 
         $this->cacheImpl = m::mock(Cache::class);
-        $this->cache->shouldReceive('driver')->once()->andReturn($this->cacheImpl);
+        $this->cache->shouldReceive('driver')
+                    ->once()->andReturn($this->cacheImpl);
 
         $this->configuration->shouldReceive('isSecondLevelCacheEnabled')
                             ->atLeast()->once()
@@ -226,13 +243,23 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
         $this->disableSecondLevelCaching();
 
         $this->config->shouldReceive('get')
-                     ->with('doctrine.cache.namespace', null)->once()
+                     ->with('doctrine.cache.namespace', null)
                      ->andReturn('namespace');
 
-        $cache = m::mock(Cache::class);
-        $this->cache->shouldReceive('driver')->once()->andReturn($cache);
+        foreach ($this->caches as $cache) {
+            $this->config->shouldReceive('get')
+                         ->with('doctrine.cache.' . $cache . '.namespace', 'namespace')
+                         ->once()
+                         ->andReturn('namespace');
+        }
 
-        $cache->shouldReceive('setNamespace')->once()->with('namespace');
+        $cache = m::mock(Cache::class);
+
+        $this->cache->shouldReceive('driver')
+                    ->times($this->cachesCount)
+                    ->andReturn($cache);
+
+        $cache->shouldReceive('setNamespace')->with('namespace');
 
         $manager = $this->factory->create($this->settings);
 
@@ -477,6 +504,18 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
     {
         $this->config = m::mock(Repository::class);
 
+        $this->config->shouldReceive('get')
+                     ->with('doctrine.cache.default', 'array')
+                     ->atLeast()->once()
+                     ->andReturn('array');
+
+        foreach ($this->caches as $cache) {
+            $this->config->shouldReceive('get')
+                         ->with('doctrine.cache.' . $cache . '.driver', 'array')
+                         ->atLeast()->once()
+                         ->andReturn('array');
+        }
+
         $this->config->shouldReceive('has')
                      ->with('database.connections.mysql')
                      ->once()
@@ -505,7 +544,9 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
     protected function mockCache()
     {
         $this->cache = m::mock(CacheManager::class);
-        $this->cache->shouldReceive('driver')->once()->andReturn(new ArrayCache());
+        $this->cache->shouldReceive('driver')
+                    ->times($this->cachesCount)
+                    ->andReturn(new ArrayCache());
     }
 
     protected function mockConnection()
@@ -565,8 +606,16 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
     protected function disableCustomCacheNamespace()
     {
         $this->config->shouldReceive('get')
-                     ->with('doctrine.cache.namespace', null)->atLeast()->once()
-                     ->andReturn(false);
+                     ->with('doctrine.cache.namespace', null)
+                     ->atLeast()->once()
+                     ->andReturn(null);
+
+        foreach ($this->caches as $cache) {
+            $this->config->shouldReceive('get')
+                         ->with('doctrine.cache.' . $cache . '.namespace', null)
+                         ->atLeast()->once()
+                         ->andReturn(null);
+        }
     }
 
     protected function disableCustomFunctions()
@@ -594,6 +643,10 @@ class EntityManagerFactoryTest extends PHPUnit_Framework_TestCase
         $this->configuration->shouldReceive('getClassMetadataFactoryName')
                             ->atLeast()->once()
                             ->andReturn('Doctrine\ORM\Mapping\ClassMetadataFactory');
+
+        $this->configuration->shouldReceive('setMetadataCacheImpl')->once();
+        $this->configuration->shouldReceive('setQueryCacheImpl')->once();
+        $this->configuration->shouldReceive('setResultCacheImpl')->once();
 
         $cache = m::mock(Cache::class);
         $this->configuration->shouldReceive('getMetadataCacheImpl')

commit da382fae494ec232f5a5838b3f8ec437f7d97e81

Fixing default caching bug introduced from #205
If you have memcached or redis extensions installed
doctrine will try to automatically connect to one of them
if no cache paramter is set when dev mode is off. Defaulting
to the default driver to prevent that behavior from occuring.

Signed-off-by: RJ Garcia <rj@bighead.net>

src/EntityManagerFactory.php
@@ -2,6 +2,7 @@
 
 namespace LaravelDoctrine\ORM;
 
+use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\Common\Cache\Cache;
 use Doctrine\ORM\Cache\DefaultCacheFactory;
 use Doctrine\ORM\Configuration;
@@ -92,9 +93,12 @@ class EntityManagerFactory
      */
     public function create(array $settings = [])
     {
+        $defaultDriver = $this->config->get('doctrine.cache.default', 'array');
+
         $configuration = $this->setup->createConfiguration(
             array_get($settings, 'dev', false),
-            array_get($settings, 'proxies.path')
+            array_get($settings, 'proxies.path'),
+            $this->cache->driver($defaultDriver)
         );
 
         $this->setMetadataDriver($settings, $configuration);