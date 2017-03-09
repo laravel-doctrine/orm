af9b0fd94327e71a0706f3d6e9602ea5a5cee6cd
Robin Lidbetter <robin@lidbetter.me>
2017-03-09 18:09:56 -0500

    fix presence verifier on lumen

--- src/Validation/PresenceVerifierProvider.php
+++ src/Validation/PresenceVerifierProvider.php
@@ -2,6 +2,7 @@
 
 namespace LaravelDoctrine\ORM\Validation;
 
+use Illuminate\Validation\Factory;
 use Illuminate\Validation\ValidationServiceProvider;
 
 class PresenceVerifierProvider extends ValidationServiceProvider
@@ -12,6 +13,27 @@ class PresenceVerifierProvider extends ValidationServiceProvider
      * @var bool
      */
     protected $defer = true;
+
+    /**
+     * Register the validation factory.
+     *
+     * @return void
+     */
+    protected function registerValidationFactory()
+    {
+        $this->app->singleton('validator', function ($app) {
+            $validator = new Factory($app['translator'], $app);
+
+            // The validation presence verifier is responsible for determining the existence of
+            // values in a given data collection which is typically a relational database or
+            // other persistent data stores. It is used to check for "uniqueness" as well.
+            if (isset($app['registry']) && isset($app['validation.presence'])) {
+                $validator->setPresenceVerifier($app['validation.presence']);
+            }
+
+            return $validator;
+        });
+    }
 
     /**
      * Register the database presence verifier.
