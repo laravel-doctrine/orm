2d59ed7c00bfea0de1695e41d8b9de0ff0b5af0a
Mateusz Koguc <mateusz.koguc@yahoo.com>
2017-09-22 08:31:42 +0100

    Add getModel() function to DoctrineUserProvider

--- src/Auth/DoctrineUserProvider.php
+++ src/Auth/DoctrineUserProvider.php
@@ -133,4 +133,14 @@ class DoctrineUserProvider implements UserProvider
 
         return $refEntity->newInstanceWithoutConstructor();
     }
+
+    /**
+     * Returns entity namespace.
+     * @return string
+     */
+    public function getModel()
+    {
+    	return $this->entity;
+    }
 }
+
