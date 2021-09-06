1acc874638c08391278dd2dd24a6905b58d650ec
Roman Naumenko <naumenko_subscr@mail.ru>
2021-09-06 16:34:28 +1000

    Fixed handling of database connections configuration for "read/write" replicas

--- tests/Configuration/Connections/PrimaryReadReplicaConnectionTest.php
+++ tests/Configuration/Connections/PrimaryReadReplicaConnectionTest.php
@@ -28,19 +28,53 @@ class PrimaryReadReplicaConnectionTest extends TestCase
         $out = [];
 
         // Case #0. Simple valid configuration with mysql base settings.
-        $out[] = [$this->getResolvedMysqlConfig(), $this->getInputConfig(), $this->getExpectedConfig()];
+        $out[] = [
+            $this->getResolvedMysqlConfig(),
+            $this->getInputConfigwithArrayOfReplicasInReadKey(),
+            $this->getExpectedConfig()
+        ];
 
         // Case #1. Configuration is only set in the read/write nodes.
-        $out[] = [['driver' => 'pdo_mysql'], $this->getNodesInputConfig(), $this->getNodesExpectedConfig()];
+        $out[] = [
+            ['driver' => 'pdo_mysql'],
+            $this->getNodesInputConfig(),
+            $this->getNodesExpectedConfig()
+        ];
 
         // Case #2. Simple valid configuration with oracle base settings.
-        $out[] = [$this->getResolvedOracleConfig(), $this->getInputConfig(), $this->getOracleExpectedConfig()];
+        $out[] = [
+            $this->getResolvedOracleConfig(),
+            $this->getInputConfigwithArrayOfReplicasInReadKey(),
+            $this->getOracleExpectedConfig()
+        ];
 
         // Case #3. Simple valid configuration with pgqsql base settings.
-        $out[] = [$this->getResolvedPgqsqlConfig(), $this->getInputConfig(), $this->getPgsqlExpectedConfig()];
+        $out[] = [
+            $this->getResolvedPgqsqlConfig(),
+            $this->getInputConfigwithArrayOfReplicasInReadKey(),
+            $this->getPgsqlExpectedConfig()
+        ];
 
         // Case #4. Simple valid configuration with sqlite base settings.
-        $out[] = [$this->getResolvedSqliteConfig(), $this->getSqliteInputConfig(), $this->getSqliteExpectedConfig()];
+        $out[] = [
+            $this->getResolvedSqliteConfig(),
+            $this->getSqliteInputConfig(),
+            $this->getSqliteExpectedConfig()
+        ];
+
+        // Case #5. Valid configuration as with 1 replica 'read' entry and plain-text host
+        $out[] = [
+            $this->getResolvedMysqlConfig(),
+            $this->getInputConfigWithPlainTextHostValue(),
+            $this->getExpectedConfigForCase5()
+        ];
+
+        // Case #6. Valid configuration as with 1 replica config 'read' entry and array of hosts in 'host' key
+        $out[] = [
+            $this->getResolvedMysqlConfig(),
+            $this->getInputConfigWithArrayAsHostValue(),
+            $this->getExpectedConfigForCase6()
+        ];
 
         return $out;
     }
@@ -64,10 +98,8 @@ class PrimaryReadReplicaConnectionTest extends TestCase
 
     /**
      * Returns dummy input configuration for testing.
-     *
-     * @return array
      */
-    private function getInputConfig()
+    private function getInputConfigwithArrayOfReplicasInReadKey(): array
     {
         return [
             'driver'    => 'mysql',
@@ -95,6 +127,70 @@ class PrimaryReadReplicaConnectionTest extends TestCase
                     'host' => 'localhost2',
                     'port' => 3309
                 ],
+            ],
+            'serverVersion'       => '5.8',
+            'defaultTableOptions' => [
+                'charset' => 'utf8mb4',
+                'collate' => 'utf8mb4_unicode_ci',
+            ]
+        ];
+    }
+
+    private function getInputConfigWithPlainTextHostValue()
+    {
+        return [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'port'      => '3306',
+            'database'  => 'test',
+            'username'  => 'homestead',
+            'password'  => 'secret',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
+            'write'     => [
+                'port'      => 3307,
+                'user'      => 'homestead1',
+                'password'  => 'secret1',
+            ],
+            'read' => [
+                'port'     => 3308,
+                'database' => 'test2',
+                'host'     => 'newhost',
+            ],
+            'serverVersion'       => '5.8',
+            'defaultTableOptions' => [
+                'charset' => 'utf8mb4',
+                'collate' => 'utf8mb4_unicode_ci',
+            ]
+        ];
+    }
+
+    private function getInputConfigWithArrayAsHostValue()
+    {
+        return [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'port'      => '3306',
+            'database'  => 'test',
+            'username'  => 'homestead',
+            'password'  => 'secret',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
+            'write'     => [
+                'port'      => 3307,
+                'user'      => 'homestead1',
+                'password'  => 'secret1',
+            ],
+            'read' => [
+                'port'     => 3308,
+                'database' => 'test2',
+                'host'     => ['newhost1', 'newhost2'],
             ],
             'serverVersion'       => '5.8',
             'defaultTableOptions' => [
@@ -136,6 +232,92 @@ class PrimaryReadReplicaConnectionTest extends TestCase
                     'unix_socket' => 'unix_socket',
                     'prefix'      => 'prefix'
                 ]
+            ],
+            'primary' => [
+                'host'        => 'localhost',
+                'user'        => 'homestead1',
+                'password'    => 'secret1',
+                'dbname'      => 'test',
+                'port'        => '3307',
+                'charset'     => 'charset',
+                'unix_socket' => 'unix_socket',
+                'prefix'      => 'prefix'
+            ],
+            'defaultTableOptions' => [
+                'charset' => 'utf8mb4',
+                'collate' => 'utf8mb4_unicode_ci',
+            ],
+        ];
+    }
+
+    /**
+     * Returns dummy expected result configuration for testing.
+     */
+    private function getExpectedConfigForCase5(): array
+    {
+        return [
+            'wrapperClass'   => PrimaryReadReplicaDoctrineWrapper::class,
+            'driver'         => 'pdo_mysql',
+            'serverVersion'  => '5.8',
+            'replica'        => [
+                [
+                    'host'        => 'newhost',
+                    'user'        => 'homestead',
+                    'password'    => 'secret',
+                    'dbname'      => 'test2',
+                    'port'        => '3308',
+                    'charset'     => 'charset',
+                    'unix_socket' => 'unix_socket',
+                    'prefix'      => 'prefix'
+                ],
+            ],
+            'primary' => [
+                'host'        => 'localhost',
+                'user'        => 'homestead1',
+                'password'    => 'secret1',
+                'dbname'      => 'test',
+                'port'        => '3307',
+                'charset'     => 'charset',
+                'unix_socket' => 'unix_socket',
+                'prefix'      => 'prefix'
+            ],
+            'defaultTableOptions' => [
+                'charset' => 'utf8mb4',
+                'collate' => 'utf8mb4_unicode_ci',
+            ],
+        ];
+    }
+
+    /**
+     * Returns dummy expected result configuration for testing.
+     */
+    private function getExpectedConfigForCase6(): array
+    {
+        return [
+            'wrapperClass'   => PrimaryReadReplicaDoctrineWrapper::class,
+            'driver'         => 'pdo_mysql',
+            'serverVersion'  => '5.8',
+            'replica'        => [
+                [
+                    'host'        => 'newhost1',
+                    'user'        => 'homestead',
+                    'password'    => 'secret',
+                    'dbname'      => 'test2',
+                    'port'        => '3308',
+                    'charset'     => 'charset',
+                    'unix_socket' => 'unix_socket',
+                    'prefix'      => 'prefix'
+                ],
+                [
+                    'host'        => 'newhost2',
+                    'user'        => 'homestead',
+                    'password'    => 'secret',
+                    'dbname'      => 'test2',
+                    'port'        => '3308',
+                    'charset'     => 'charset',
+                    'unix_socket' => 'unix_socket',
+                    'prefix'      => 'prefix'
+                ],
             ],
             'primary' => [
                 'host'        => 'localhost',
@@ -316,7 +498,7 @@ class PrimaryReadReplicaConnectionTest extends TestCase
      */
     private function getSqliteInputConfig()
     {
-        $inputConfigSqlite = $this->getInputConfig();
+        $inputConfigSqlite = $this->getInputConfigwithArrayOfReplicasInReadKey();
         unset($inputConfigSqlite['read'][0]['database']);
         unset($inputConfigSqlite['read'][1]['database']);
         unset($inputConfigSqlite['write']['database']);
