52ebb7eed4308054c4702e2812d0a9d7f3a875fa
František Mazura <mazura@ogsoft.cz>
2021-09-21 07:32:11 +0200

    coding style fix

--- src/Loggers/LaravelEventLogger.php
+++ src/Loggers/LaravelEventLogger.php
@@ -2,18 +2,19 @@
 
 namespace LaravelDoctrine\ORM\Loggers;
 
-use LaravelDoctrine\ORM\Loggers\SQL\DoctrineSQLLogger;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManagerInterface;
+use LaravelDoctrine\ORM\Loggers\SQL\DoctrineSQLLogger;
 
 class LaravelEventLogger implements Logger
 {
-	/**
-	 * @param EntityManagerInterface $em
-	 * @param Configuration $configuration
-	 */
-	public function register(EntityManagerInterface $em, Configuration $configuration) {
-		$logger = new DoctrineSQLLogger($em, $configuration);
-		$configuration->setSQLLogger($logger);
-	}
+    /**
+     * @param EntityManagerInterface $em
+     * @param Configuration          $configuration
+     */
+    public function register(EntityManagerInterface $em, Configuration $configuration)
+    {
+        $logger = new DoctrineSQLLogger($em, $configuration);
+        $configuration->setSQLLogger($logger);
+    }
 }
