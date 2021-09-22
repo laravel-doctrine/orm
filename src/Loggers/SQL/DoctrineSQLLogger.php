18ac1bef240fe6c97bd3a4c0883be467b54c0fab
František Mazura <mazura@ogsoft.cz>
2021-09-22 11:01:45 +0200

    add Laravel Connection support

--- src/Loggers/SQL/DoctrineSQLLogger.php
+++ src/Loggers/SQL/DoctrineSQLLogger.php
@@ -5,61 +5,61 @@ namespace LaravelDoctrine\ORM\Loggers\SQL;
 use Doctrine\DBAL\Logging\SQLLogger;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Illuminate\Database\Connection;
 use Illuminate\Database\Events\QueryExecuted;
+use LaravelDoctrine\ORM\Exceptions\NotImplemented;
+use PDO;
 
 class DoctrineSQLLogger implements SQLLogger
 {
-    /** @var float|null */
-    public $start = null;
+	/** @var float|null */
+	public $start = null;
 
-    /** @var string|null */
-    public $query = null;
+	/** @var string|null */
+	public $query = null;
 
-    /** @var array|null */
-    public $params = null;
+	/** @var array|null */
+	public $params = null;
 
-    /**
-     * @var EntityManagerInterface
-     */
-    public $em;
-    /**
-     * @var Configuration
-     */
-    public $configuration;
+	/**
+	 * @var EntityManagerInterface
+	 */
+	public $em;
+	/**
+	 * @var Configuration
+	 */
+	public $configuration;
 
-    public function __construct(EntityManagerInterface $em, Configuration $configuration)
-    {
-        $this->em            = $em;
-        $this->configuration = $configuration;
-    }
+	public function __construct(EntityManagerInterface $em, Configuration $configuration)
+	{
+		$this->em = $em;
+		$this->configuration = $configuration;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function startQuery($sql, ?array $params = null, ?array $types = null)
-    {
-        $this->start  = microtime(true);
-        $this->query  = $sql;
-        $this->params = $params;
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function startQuery($sql, ?array $params = null, ?array $types = null)
+	{
+		$this->start = microtime(true);
+		$this->query = $sql;
+		$this->params = $params;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function stopQuery()
-    {
-        $executionTime = microtime(true) - $this->start;
-        // TODO get connection name
-        // this class is Laravel connection wrapper (QueryExecuted needs getName())
-        $object                    = new class {
-            public $connectionName = "";
+	/**
+	 * {@inheritdoc}
+	 */
+	public function stopQuery()
+	{
+		$executionTime = microtime(true) - $this->start;
+		$wreppedConnection = $this->em->getConnection()->getWrappedConnection();
+		if (!$wreppedConnection instanceof PDO) {
+			throw new Exception("Only PDO is supported");
+		}
+		$connection = new Connection($wreppedConnection);
+		$event = new QueryExecuted($this->query, $this->params, $executionTime, $connection);
 
-            public function getName()
-            {
-                return $this->connectionName;
-            }
-        };
-        $event = new QueryExecuted($this->query, $this->params, $executionTime, $object);
-        event($event);
-    }
+		event($event);
+	}
 }
