54f9b985daecfef54be8343724f254b3d8b4bbce
František Mazura <mazura@ogsoft.cz>
2021-09-24 09:17:58 +0200

    add tests

---
+++ tests/Loggers/LaravelEventLoggerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManagerInterface;
+use Illuminate\Events\Dispatcher;
+use LaravelDoctrine\ORM\Loggers\LaravelEventLogger;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+
+class LaravelEventLoggerTest extends TestCase
+{
+    public function test_can_register()
+    {
+        $writer = m::mock(Dispatcher::class);
+        $em = m::mock(EntityManagerInterface::class);
+        $configuration = m::mock(Configuration::class);
+
+        $configuration->shouldReceive('setSQLLogger')
+            ->once();
+
+        $em->shouldReceive('getConnection')
+            ->once()->andReturn(m::mock(Connection::class));
+
+        $logger = new LaravelEventLogger($writer);
+
+        $logger->register($em, $configuration);
+
+        $this->assertTrue(true);
+    }
+
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+}
---
+++ tests/Loggers/SQL/DoctrineSQLLoggerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use Doctrine\DBAL\Connection;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Events\Dispatcher;
+use LaravelDoctrine\ORM\Loggers\SQL\DoctrineSQLLogger;
+use Mockery as m;
+use Mockery\Mock;
+use PHPUnit\Framework\TestCase;
+
+class DoctrineSQLLoggerTest extends TestCase
+{
+    /**
+     * @var DoctrineSQLLogger
+     */
+    protected $logger;
+
+    /**
+     * @var Dispatcher
+     */
+    protected $dispatcher;
+
+    /**
+     * @var Mock
+     */
+    protected $connection;
+
+    protected function getConnectionMock()
+    {
+        $mock = $this->getMockBuilder(Connection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->expects($this->any())
+            ->method('getWrappedConnection')
+            ->will($this->returnValue(new PDO('sqlite::memory:')));
+
+        return $mock;
+    }
+
+
+    protected function setUp(): void
+    {
+        $this->connection = $this->getConnectionMock();
+        $this->dispatcher = new Dispatcher();
+
+        $this->logger = new DoctrineSQLLogger(
+            $this->connection,
+            $this->dispatcher,
+        );
+    }
+
+    public function test_transforms_query_to_event()
+    {
+        $query = 'SELECT * FROM table WHERE condition = ?';
+        $params = ['value'];
+
+        $this->dispatcher->listen(QueryExecuted::class, function ($event) use ($query, $params) {
+            $this->assertInstanceOf(QueryExecuted::class, $event);
+            $this->assertEquals($query, $event->sql);
+            $this->assertEquals($params, $event->bindings);
+            $this->assertGreaterThan(0, $event->time);
+        });
+
+
+        $this->logger->startQuery($query, $params);
+        usleep(10);
+        $this->logger->stopQuery();
+    }
+
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+}
