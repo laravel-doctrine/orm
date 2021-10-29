35117d3598931cee9e675c2c299756e25be7bbde
Mariusz Soltys <14175459+soltmar@users.noreply.github.com>
2021-10-29 13:16:25 +0100

    Better IDE support for entity() parameters (#504)

--- src/helpers.php
+++ src/helpers.php
@@ -4,15 +4,13 @@ if (!function_exists('entity')) {
     /**
      * Create a model factory builder for a given class, name, and amount.
      *
-     * @param  dynamic  class|class,name|class,amount|class,name,amount
+     * @param mixed ...$arguments class|class,name|class,amount|class,name,amount
      *
      * @return \LaravelDoctrine\ORM\Testing\FactoryBuilder
      */
-    function entity()
+    function entity(...$arguments)
     {
         $factory = app('LaravelDoctrine\ORM\Testing\Factory');
-
-        $arguments = func_get_args();
 
         if (isset($arguments[1]) && is_string($arguments[1])) {
             return $factory->of($arguments[0], $arguments[1])->times(isset($arguments[2]) ? $arguments[2] : 1);
