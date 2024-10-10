fdbbc69346b767bf087691e07731b20069f765a9
Tom H Anderson <tom.h.anderson@gmail.com>
2024-10-09 23:29:33 -0600

    phpstan level 0

--- tests/helpers.php
+++ tests/helpers.php
@@ -9,3 +9,11 @@ function app($param) {}
 function database_path($path = '') {
     return '';
 }
+
+function config_path($path = '') {
+    return $path;
+}
+
+function base_path($path = '') {
+    return $path;
+}
